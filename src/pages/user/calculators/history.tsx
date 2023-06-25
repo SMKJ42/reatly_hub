20fcf7188443c7b27b1c9415233f4633c82b2039
SKMJ42 <williamrdeason1@gmail.com>
2023-06-25 11:09:35 -0400

    pulled data from SFH into history, set up display for POD

--- src/pages/user/calculators/history.tsx
+++ src/pages/user/calculators/history.tsx
@@ -3,6 +3,7 @@ import UserLayout from "../../../components/layouts/UserLayout";
 import type { NextPageWithLayout } from "../../_app";
 import { useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
+import { LoadingSpinner } from "~/components/shared/LoadingSpinner";
 
 const Dashboard: NextPageWithLayout = () => {
   const user = useUser();
@@ -10,9 +11,24 @@ const Dashboard: NextPageWithLayout = () => {
   const { data, isLoading: historyLoading } =
     api.singleFamily.getAll.useQuery();
 
+  if (historyLoading) return <LoadingSpinner />;
+
+  console.log(data);
+
   return (
     <div className="">
       <h1 className=""> History </h1>
+      {data?.map((pod) => {
+        return (
+          <div key={pod.id} className="pod-container">
+            <h3 className="">{pod.address}</h3>
+            <p className="">Monthly cash flow: ${pod.cashFlow}</p>
+            <p className="">COC: {pod.cashOnCash}%</p>
+            <p className="">ROE: {pod.ROE}%</p>
+            <p className="">ROI: {pod.ROI}%</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
